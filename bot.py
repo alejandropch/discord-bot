95e82ed7f664affc6dabf561679a9f2d02096e17
Alejandro Pachas <alejandropachas1@gmail.com>
2022-07-12 10:36:41 -0500

    Added participant request to bot

--- bot.py
+++ bot.py
@@ -14,5 +14,23 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print('Connected as {0.user}'.format(client))
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    else:
+        participant = {
+            'id': str(message.author.id),
+            'avatar_url': message.author.avatar_url,
+            'name': message.author.name,
+            'display_name': message.author.display_name,
+            'discriminator': message.author.discriminator,
+            'joined_at': message.author.joined_at,
+        }
+
+        req = requests.post(os.environ["API_URL"] + '/api/participant', data = json.dumps(participant), headers = {
+            'Content-Type': 'aplication/json'
+        })
+
 
 client.run(bot_token)
