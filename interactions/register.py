5d4fcadd96b0568792d84b01e438c7136c69df2a
Alejandro Pachas <alejandropachas1@gmail.com>
2022-09-07 14:30:37 -0500

    now a message is returned whether if the user exists in the db or not, also, the userExists method is called

--- interactions/register.py
+++ interactions/register.py
@@ -3,17 +3,11 @@ from utils.userExists import handle as userExists
 from utils.User import User
 
 
+async def handle(interaction: discord.Interaction, season: str, winner: User):
 
-    value = requests.post(os.environ["API_URL"] + '/participants/', data=json.dumps(data), headers={
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + os.environ["API_KEY"]
-    })
-    print(value.json())
+    exists = await userExists(str(interaction.user.id))
+    if exists is None:
+        await winner.setRemanaingData(interaction, season)
+        return [f"Thanks for registering - I sent you a direct message, please check settings if you did not receive it.", False]
 
-    try:
-        value.json()
-        print(value.text)
-    except json.decoder.JSONDecodeError:
-        print("json empty")
-
-    return
+    return [f"Hey {interaction.user.name}, seems that we tried to register you again. Buy you're already on our database!. Sorry ☹️", True]
