304ac12f03152aa1a6b4d5c3252e098c8e558d13
Alejandro Pachas <alejandropachas1@gmail.com>
2022-09-07 13:29:46 -0500

    adding documentation to the userExists (handle) function

--- utils/userExists.py
+++ utils/userExists.py
@@ -6,7 +6,7 @@ import json
 
 
 async def handle(discord_id):
-
+    """ It will return None if the user does not exists in the db """
     value = requests.get(os.environ["API_URL"] + f'/participants/{discord_id}', headers={
         'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + os.environ["API_KEY"]
