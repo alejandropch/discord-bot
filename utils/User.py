9841a1161b1de95c006a72e331f1bbd2e7002beb
Alejandro Pachas <alejandropachas1@gmail.com>
2022-09-09 09:39:20 -0500

    the only attribute is sended is what the user response, so its gonna be the same one is cleared in the clear method

--- utils/User.py
+++ utils/User.py
@@ -7,21 +7,14 @@ class User(FormManager):
 
     def __init__(self, client):
         super().__init__(client)
-        self.name = ''
-        self.lastName = ''
-        self.address1 = ''
-        self.address2 = ''
-        self.city = ''
-        self.state = ''
-        self.postalCode = ''
-        self.discordUsername = ''
-        self.country = ''
-        self.discord_id = ''
-        self.avatar_url = ''
+        self.response = []
         self.isWinner = False
 
-    async def setRemanaingData(self, interaction: discord.Interaction, season: str):
-        """ this function sets the remaining data from discord that must be sent to the CE admin"""
+    async def setRemanaingData(self, interaction: discord.Interaction, fields):
+        """ this function sets the remaining data from discord and the CE Admin """
+
+        self.questions = list(map(lambda x: x['question'], fields))
+        self.nQuestions = len(self.questions)
 
         self.isWinner = True
 
@@ -34,7 +27,7 @@ class User(FormManager):
             interaction.user.avatar.url if interaction.user.avatar is not None else '')
         self.discord_id = str(interaction.user.id)
 
-        self.userObject = await self.client.fetch_user(self.discord_id)
+        self.userObj = await self.client.fetch_user(self.discord_id)
         # TODO: put this season id into the pivot table
 
     async def dmUser(self):
@@ -46,78 +39,17 @@ class User(FormManager):
 
             response = self.errorHandler(message.content)
             return False if response is False else True
-
-        if self.i == 0:
-            await self.userObject.send("What is your first name?")
-
-            name = (await self.client.wait_for("message", check=check)).content
-            if name == "":
-                return
-
-            self.name = name or ''
-
-        if self.i == 1:
-            await self.userObject.send("What is your last name?")
-            lastName = (await self.client.wait_for("message", check=check)).content
-            if lastName == "":
-                return
-            self.lastName = lastName or ''
-
-        if self.i == 2:
-            await self.userObject.send("Please provide your Address (first line)")
-            address1 = (await self.client.wait_for("message", check=check)).content
-            if address1 == "":
-                return
-            self.address1 = address1 or ''
-
-        if self.i == 3:
-            await self.userObject.send("Please provide your Address 2 (if none, please type '-')")
-            address2 = (await self.client.wait_for("message", check=check)).content
-
-            if address2 == "" or address2.strip() == "-":
-                return
-            self.address2 = address2 or ''
-
-        if self.i == 4:
-            await self.userObject.send("Please provide your City")
-            city = (await self.client.wait_for("message", check=check)).content
-            if city == "":
-                return
-            self.city = city or ''
-
-        if self.i == 5:
-            await self.userObject.send("Please provide your State/Province")
-            state = (await self.client.wait_for("message", check=check)).content
-            if state == "":
-                return
-            self.state = state or ''
-
-        if self.i == 6:
-            await self.userObject.send("Please provide your Postal Code")
-            postalCode = (await self.client.wait_for("message", check=check)).content
-            if postalCode == "":
-                return
-            self.postalCode = postalCode or ''
-
-        if self.i == 7:
-            await self.userObject.send("Please provide your Country")
-            country = (await self.client.wait_for("message", check=check)).content
-            if country == "":
-                return
-            self.country = country or ''
-
-        if self.i == 8:
+        # if the iterator have the same value of the total n° of questions
+        if self.i >= self.nQuestions:
             view = YorNButtons(self)
             await self.formOver(self, view)
+            return
+
+        await self.userObj.send(self.questions[self.i])
+        res = (await self.client.wait_for("message", check=check)).content
+        self.response.append(res or '')
 
     def clear(self):
         """ restart the iterator and the attributes of the winner user """
-        self.name = ''
-        self.lastName = ''
-        self.address1 = ''
-        self.address2 = ''
-        self.city = ''
-        self.state = ''
-        self.postalCode = ''
-        self.country = ''
+        self.response.clear()
         self.i = 0
