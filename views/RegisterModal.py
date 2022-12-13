374b0faa3d9fccbc36d4892b77ea595f61cecd21
Alejandro Pachas <alejandropachas1@gmail.com>
2022-12-13 16:29:42 -0500

    adding a parameter to the RegisterModal class and sending an interaction.response depeding of the new parameter value

--- views/RegisterModal.py
+++ views/RegisterModal.py
@@ -3,19 +3,16 @@ from discord.ext import commands
 from discord import app_commands
 
 class RegisterModal(discord.ui.Modal):
-    def __init__(self,title = 'Registration Proccess!', participant=object):
+    def __init__(self,title = 'Registration Proccess!', participant=object, one_season=bool):
         super().__init__(title = title)
         self.participant=participant
+        self.one_season=one_season
         self.start()
     
     def start (self):
         for question in self.participant.questions:
-        
             answer =discord.ui.TextInput(label = question['question'], style = discord.TextStyle.short,required = True)
-            
-            
             self.add_item(answer)
-            
             self.participant.saveResponse(answer)
 
 
@@ -32,11 +29,15 @@ class RegisterModal(discord.ui.Modal):
             response = await self.participant.handleRequest(self.participant)
             embed = discord.Embed(title = self.title, description = f"{self.participant.getOutputResult(self.participant)}**{response}**")
             embed.set_author(name = interaction.user, icon_url = interaction.user.avatar)
-            await interaction.response.send_message(embed = embed, ephemeral = True)
+            if self.one_season == True:
+                await interaction.response.send_message(embed = embed, ephemeral = True)
+            else:
+                await interaction.response.edit_message(embed = embed, content = None, view = None)
+
             self.participant.clear()
 
         except Exception as err:
             print(err)
-            await interaction.response.send_message(content=f"Something went wrong!!")
+            await interaction.response.send_message(content="Something went wrong!!",ephemeral = True)
         
 
