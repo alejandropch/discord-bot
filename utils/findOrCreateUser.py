3c705668281e48facf57f13a4497ca9a8f2d942b
Alejandro Pachas <alejandropachas1@gmail.com>
2023-01-04 18:43:34 -0500

    check function logic was split using get and post request on diferent api routes

--- utils/findOrCreateUser.py
+++ utils/findOrCreateUser.py
@@ -6,25 +6,31 @@ import json
 
 
 async def handle(interaction):
-    data={
-        "discord_id": str(interaction.user.id),
-        "discord_username": interaction.user.name + '#' + interaction.user.discriminator,
-        "avatar_url": str(interaction.user.avatar.url if interaction.user.avatar is not None else '')
-    }
 
     ## Only to find if the records exists as a participant
-    value = requests.get(os.environ["API_URL"] + f'/participants/' + str(interaction.user.id), headers={
+    x_find = requests.get(os.environ["API_URL"] + f'/participants/' + str(interaction.user.id), headers={
         'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + os.environ["API_KEY"]
     })
     # This line decodes the response from the request as JSON and assigns it to a variable
-    res = json.loads(value.text)
-    if res['status'] != 'success':
+    find = json.loads(x_find.text)
+    if find['status'] != 'success':
+        
+        data={
+            "discord_id": str(interaction.user.id),
+            "discord_username": interaction.user.name + '#' + interaction.user.discriminator,
+            "avatar_url": str(interaction.user.avatar.url if interaction.user.avatar is not None else '')
+        }
         ## Create as a Participant
-        value = requests.post(os.environ["API_URL"] + f'/participants/', data, headers={
+        x_create = requests.post(os.environ["API_URL"] + f'/participants/', data= json.dumps(data), headers={
             'Content-Type': 'application/json',
             'Authorization': 'Bearer ' + os.environ["API_KEY"]
         })
-        res = json.loads(value.text)
+
+        create = json.loads(x_create.text)
+        print(create)
+
+        return create
+    
     # This line returns the JSON response
-    return res
+    return find
