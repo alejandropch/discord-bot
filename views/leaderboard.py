0de1319909b8ffdcf3078b1301b1e9a81ae6649b
Alejandro Pachas <alejandropachas1@gmail.com>
2023-01-16 15:47:38 -0500

    not passing the hole interaction variable, just the user.id (discord_id) instead

--- views/leaderboard.py
+++ views/leaderboard.py
@@ -22,7 +22,7 @@ class SeasonButtons(discord.ui.View):
     def generate_callback(self, option: str):
         
         async def validate_button(interaction: discord.Interaction):
-                response = await handleLeaderboard(interaction, option['id'])
+                response = await handleLeaderboard(interaction.user.id, option['id'])
                 embed = discord.Embed(title = option['name'], description = response['message'])
                 embed.set_author(name = interaction.user, icon_url = interaction.user.avatar)
                 await interaction.response.edit_message(embed = embed, content = None, view = None)
