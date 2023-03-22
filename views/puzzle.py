5be8265dbf1e9f8fd5482835d5fa1296e451b527
Alejandro Pachas <alejandropachas1@gmail.com>
2023-03-22 11:46:27 -0500

    [views/puzzle.py] changing/adding titles

--- views/puzzle.py
+++ views/puzzle.py
@@ -59,13 +59,13 @@ class Button(discord.ui.View):
     def generate_callback(self):
 
         async def validate_button(interaction: discord.Interaction):
-            await interaction.response.send_modal(PuzzleModal(event=self.event))
+            await interaction.response.send_modal(PuzzleModal(title= self.event['name'], event=self.event))
 
         return validate_button
 
 class PuzzleModal(discord.ui.Modal):
-    def __init__(self,  event = ''):
-        super().__init__()
+    def __init__(self, title = 'Puzzle!',  event = ''):
+        super().__init__(title = title)
         self.event = event
 
     answer = discord.ui.TextInput(label = 'Answer', style = discord.TextStyle.short, required = True)
@@ -73,7 +73,7 @@ class PuzzleModal(discord.ui.Modal):
     async def on_submit(self, interaction: discord.Interaction):
         
         response = await handleTrivia(interaction, self.event, self.answer.value)
-        embed = discord.Embed(title = 'Write your answer!', description = f"**{self.answer.label}**\n{self.answer}\n\n**{response}**")
+        embed = discord.Embed(title = self.event['name'], description = f"**{self.answer.label}**\n{self.answer}\n\n**{response}**")
         embed.set_author(name = interaction.user, icon_url = interaction.user.avatar)
         await interaction.response.send_message(embed = embed, ephemeral = True)
 
